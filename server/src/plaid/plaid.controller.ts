fa8e51ae6e4b2f478a35db2365e2fa7031eb95ed
Shilendra Singh <business.shilendra@gmail.com>
2025-12-05 14:55:58 +0530

    implement Plaid service and controller for link token creation

--- server/src/plaid/plaid.controller.ts
+++ server/src/plaid/plaid.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PlaidService } from './plaid.service';
 
 @Controller('plaid')
-export class PlaidController {}
+export class PlaidController {
+  constructor(private readonly plaidService: PlaidService) {}
+
+  @Get('create_link_token')
+  async createLinkToken() {
+    const link_token = await this.plaidService.createLinkToken();
+    return { link_token };
+  }
+}
--- server/src/plaid/plaid.service.ts
+++ server/src/plaid/plaid.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import {
+  Configuration,
+  PlaidApi,
+  PlaidEnvironments,
+  CountryCode,
+  Products,
+} from 'plaid';
 
 @Injectable()
-export class PlaidService {}
+export class PlaidService {
+  private client: PlaidApi;
+
+  constructor() {
+    const config = new Configuration({
+      basePath: PlaidEnvironments.sandbox, // or development / production
+      baseOptions: {
+        headers: {
+          'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+          'PLAID-SECRET': process.env.PLAID_SECRET,
+        },
+      },
+    });
+
+    this.client = new PlaidApi(config);
+  }
+
+  async createLinkToken(): Promise<string> {
+    const requestPayload = {
+      user: {
+        client_user_id: 'unique_user_id',
+      },
+      client_name: 'plaid-trail',
+      products: [Products.Auth],
+      language: 'en',
+      webhook: 'https://www.plaid.com/webhook',
+      redirect_uri: 'https://loacalhost:5173/',
+      country_codes: [CountryCode.Us],
+    };
+
+    try {
+      const response = await this.client.linkTokenCreate(requestPayload);
+      return response.data.link_token;
+    } catch (error) {
+      console.error('Error creating link token:', error);
+      throw error;
+    }
+  }
+}
